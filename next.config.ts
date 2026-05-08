9bf39848964950dcaddd069b663f9c6359a05564
Alex Rocha <aerocha56@gmail.com>
2026-05-07 20:05:34 -0600

    infinityGallery_v10.6(darkThemeforAll)

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
     experimental: {
         proxyClientMaxBodySize: 50 * 1024 * 1024,
     },
+    // @ts-ignore
+    allowedDevOrigins: ['unsavory-eternal-wired.ngrok-free.dev'],
+
     images: {
         remotePatterns: [
             {
